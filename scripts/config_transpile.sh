f77f93a6c106092e0a6abb00b91dab8404145301
Ryuma Yoshida <ryuma.y1117@gmail.com>
2019-01-22 15:04:05 +0900

    Fix script

--- scripts/config_transpile.sh
+++ scripts/config_transpile.sh
@@ -5,5 +5,6 @@ set -eu
 readonly BASE_DIR=$(cd $(dirname $0); pwd)
 
 ct -pretty -strict \
+    -platform vagrant-virtualbox \
     -in-file ${BASE_DIR}/../cl-config/cl-config.yaml \
     -out-file ${BASE_DIR}/../cl-config/ignition-config.json
